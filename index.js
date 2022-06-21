b9617bd6cde2bbc79a4c760abd8c6881fe444613
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-06-21 12:59:46 +0530

    added code

---
+++ index.js
@@ -0,0 +1,9 @@
+// Viliian name generator
+var superv = require('supervillains');
+var myv = superv.random();
+console.log("Your villain : ", myv);
+
+// Hero name generator
+var superh = require('superheroes');
+var myh = superh.random();
+console.log("Your hero : ", myh);
